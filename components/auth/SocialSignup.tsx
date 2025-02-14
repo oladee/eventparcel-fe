a273d3ec1993b04a3f8cb2e5274f302dee0411ba
henrytrust1111 <henrytrust1111@gmail.com>
2025-02-14 11:59:11 +0100

    terms and conditions13

--- components/auth/SocialSignup.tsx
+++ components/auth/SocialSignup.tsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { Facebook, Google, Apple } from '../icons/Icons'
+import axios from 'axios';
 
 const SocialSignup: React.FC = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const token = urlParams.get("token");
+  console.log(token);
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    const handleAuthentication = async () => {
+      try {
+        const response = await axios.get(
+          "https://api-eventparcel.onrender.com/auth/user", { token },
+          // {
+          //   headers: {
+          //     Authorization: `Bearer ${token}`,
+          //   },
+          // }
+        );
+        if (response.data && response.data.data) {
+          const user = response.data.data;
+          console.log(user);
+          localStorage.setItem("personInfo", JSON.stringify(user));
+        }
+      } catch (error: any) {
+        console.error("Error during authentication:", error.message);
+      }
+    };
+
+    // const timer = setTimeout(() => {
+    //   handleAuthentication();
+    // }, 10000);
+
+    // return () => clearTimeout(timer);
+    handleAuthentication();
+  }, [token]);
+
   return (
     <div className="grid lg:grid-cols-3 gap-4">
-    <button className="authButton">
-      <Google width={20} height={20} /> Google
-    </button>
-    <button className="authButton">
-      <Facebook width={20} height={20} /> Facebook
-    </button>
-    <button className="authButton">
-      <Apple width={20} height={20} /> Apple
-    </button>
-  </div>
+      <a href='https://api-eventparcel.onrender.com/auth/google' className="authButton">
+        <Google width={20} height={20} /> Google
+      </a>
+      <a href='https://api-eventparcel.onrender.com/auth/facebook' className="authButton">
+        <Facebook width={20} height={20} /> Facebook
+      </a>
+      <button className="authButton">
+        <Apple width={20} height={20} /> Apple
+      </button>
+    </div>
   )
 }
 
