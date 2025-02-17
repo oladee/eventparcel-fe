ab772ca5fbfc1b3a16c63cb0ee29be14bfc9083c
henrytrust1111 <henrytrust1111@gmail.com>
2025-02-17 09:49:53 +0100

    signUp Check

--- components/PhoneNumberInput.tsx
+++ components/PhoneNumberInput.tsx
@@ -43,7 +43,7 @@ const PhoneNumberInput = ({ onPhoneChange }: {onPhoneChange: (value: string) => 
         onChange={handlePhoneChange}
         enableSearch
         disableDropdown={false}
-        onFocus={(_, data) => setTouched(true)}
+        onFocus={() => setTouched(true)}
         inputClass="!w-full !py-6 !border-none !rounded-[12px] !bg-gray-100 !focus:outline-none !focus:ring-2 !focus:ring-gray-300"
         buttonClass="!border-none !rounded-l-[12px]"
       />
