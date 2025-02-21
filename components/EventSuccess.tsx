f541a5f993b4e3e7b01aaad3ce6d62f95035ee24
henrytrust1111 <henrytrust1111@gmail.com>
2025-02-21 15:38:11 +0100

    about

--- components/EventSuccess.tsx
+++ components/EventSuccess.tsx
@@ -7,7 +7,7 @@ const EventSuccess: React.FC = () => {
     <div className="fixed p-4 w-screen inset-0  overflow-y-auto  bg-[#00000098] h-screen left-0 top-0 z-30 flex items-center justify-center md:justify-center px-2">
       <div className="bg-white p-6 rounded-[24px] w-full max-w-md grid place-items-center text-center gap-4">
         <Checked width={100} height={100} />
-        <p className="font-bold text-3xl">
+        <p className="font-bold text-2xl md:text-3xl">
           You’ve successfully created an event
         </p>
         <p className="font-medium text-[#718096]">
