6b3c381f0e66a3f19685859aa7f5508c54b7195e
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-10 16:53:05 +0100

    login

--- components/auth/SocialSignup.tsx
+++ components/auth/SocialSignup.tsx
@@ -38,15 +38,15 @@ const SocialSignup: React.FC = () => {
   }, []);
 
   return (
-    <div className="grid lg:grid-cols-3 gap-4">
-      <a href='https://api-eventparcel.onrender.com/auth/google' className="authButton">
-        <Google width={20} height={20} /> Google
+    <div className="grid grid-cols-3 gap-4">
+      <a href='https://api-eventparcel.onrender.com/auth/google' className="authButton w-max">
+        <Google width={20} height={20} /> <span className='hidden sm:block'>Google</span>
       </a>
-      <a href='https://api-eventparcel.onrender.com/auth/facebook' className="authButton">
-        <Facebook width={20} height={20} /> Facebook
+      <a href='https://api-eventparcel.onrender.com/auth/facebook' className="authButton w-max">
+        <Facebook width={20} height={20} /> <span className='hidden sm:block'>Facebook</span>
       </a>
-      <button className="authButton">
-        <Apple width={20} height={20} /> Apple
+      <button className="authButton w-max">
+        <Apple width={20} height={20} /> <span className='hidden sm:block'>Apple</span>
       </button>
     </div>
   );
