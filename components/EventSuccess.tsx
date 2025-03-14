6ea9066bdfd00ec257e21f7b0560e5ecdd326335
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-14 08:26:34 +0100

    auth

--- components/EventSuccess.tsx
+++ components/EventSuccess.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Checked } from "../components/icons/Icons";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const EventSuccess: React.FC = () => {
+  const router = useRouter();
+
+  const handleAddCoHost = () => {
+    const authToken = localStorage.getItem("authToken");
+    if (authToken) {
+      router.push("/add-cohost");
+    } else {
+      router.push("/");
+    }
+  };
   return (
     <div className="fixed p-4 w-screen inset-0  overflow-y-auto  bg-[#00000098] h-screen left-0 top-0 z-30 flex items-center justify-center md:justify-center px-2">
       <div className="bg-white p-6 rounded-[24px] w-full  max-w-md grid place-items-center text-center gap-4">
@@ -17,12 +28,13 @@ const EventSuccess: React.FC = () => {
         <Link href="/new-group" className="button_v1">
           <button className="">Continue to Group</button>
         </Link>
-        <Link
-          href="/create-cohost"
+
+        <button
+          onClick={handleAddCoHost}
           className="p-3 border border-[#111827] rounded-[12px] font-manrope font-extrabold text-base text-[#111827] w-full"
         >
-          <button className="">Add a Co-Host</button>
-        </Link>
+          Add a Co-Host
+        </button>
       </div>
     </div>
   );
