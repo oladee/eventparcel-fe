07ec05b8e1dc1e624480868422a087fffa4523ef
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-14 10:57:36 +0100

    added withCredentialsTrue3

--- components/EventSuccess.tsx
+++ components/EventSuccess.tsx
@@ -21,15 +21,14 @@ const EventSuccess: React.FC = () => {
   console.log(window.location.pathname);
   
   return (
-    <div className="fixed p-4 w-screen inset-0  overflow-y-auto  bg-[#00000098] h-screen left-0 top-0 z-30 flex items-center justify-center md:justify-center px-2">
+    <div className="fixed py-4 !px-4 w-screen inset-0  overflow-y-auto  bg-[#00000098] h-screen left-0 top-0 z-30 flex items-center justify-center md:justify-center">
       <div className="bg-white p-6 rounded-[24px] w-full  max-w-md grid place-items-center text-center gap-4">
         <Checked width={100} height={100} />
         <p className="font-bold text-2xl md:text-3xl">
           You&apos;ve successfully created an event
         </p>
         <p className="font-medium text-[#718096]">
-          You can now create groups for different type of guest and packages to
-          sell according to group
+        Would you like to add a co-host to this event or continue with the group creation?
         </p>
         <Link href="/new-group" className="button_v1">
           <button className="">Continue to Group</button>
