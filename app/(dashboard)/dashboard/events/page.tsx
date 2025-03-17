f8d096a964651799efa6d824e2b5dc278497e018
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-17 18:26:14 +0100

    structuring the sidebar

--- app/(dashboard)/dashboard/events/page.tsx
+++ app/(dashboard)/dashboard/events/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full h-screen flex items-center justify-center'>
+    <div className='w-full mt-[200px] flex items-center justify-center'>
       Events Page is Coming soon
     </div>
   )
