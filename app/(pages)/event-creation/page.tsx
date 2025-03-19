ead4d3f1df20b5cd01c3065c1a3ff29120b270c3
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-19 16:21:48 +0100

    events is ongoing1

--- app/(pages)/event-creation/page.tsx
+++ app/(pages)/event-creation/page.tsx
@@ -61,7 +61,7 @@ const PageContent: React.FC = () => {
       // Consume the profile endpoint
       const fetchUserProfile = async () => {
         try {
-          const response = await axiosInstance.post("/profile", {
+          const response = await axiosInstance.post("/profile-details", {
             token: authToken
           });
           // Save the response to localStorage as the logged-in user
