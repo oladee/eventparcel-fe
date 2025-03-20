db4f25165c37e2a89cb521651b1ba654c3f928dd
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-20 16:28:29 +0100

    fixing the loggInUserDetail

--- app/(pages)/event-creation/page.tsx
+++ app/(pages)/event-creation/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Suspense, useState, useRef, useEffect } from "react";
 import EventSuccess from "@/components/EventSuccess";
 import ImagePickerModal from "@/components/aboutEvent/ImagePickerModal";
