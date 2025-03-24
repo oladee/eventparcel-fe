3014417c1afd758e6328cc2263441d8ae6d3962f
henrytrust1111 <henrytrust1111@gmail.com>
2025-03-24 14:12:32 +0100

    edit event

--- components/dashboard/eventComponents/FormButton3.tsx
+++ components/dashboard/eventComponents/FormButton3.tsx
@@ -15,7 +15,7 @@ const FormButtons3: React.FC<FormButtonsProps> = ({
   loading,
   loading2,
 }) => (
-  <div className="bg-[#FFFF] py-3 flex justify-center">
+  <div className="bg-[#FFFF] pb-6 md:pb-3 pt-3 flex justify-center">
     <div className="max-w-screen-md flex gap-4 items-center justify-center sm:justify-end w-full px-3 lg:px-0">
       <button disabled={!isFormValid || loading} onClick={onContinue2} className="p-3 border border-[#111827] rounded-[12px] font-manrope font-extrabold text-base text-[#111827]">
        
@@ -23,7 +23,7 @@ const FormButtons3: React.FC<FormButtonsProps> = ({
       </button>
       <button
         disabled={!isFormValid || loading}
-        className={`bg-primary text-white pt-3 pb-6 md:pb-3 px-8 rounded-[12px] transition flex items-center justify-center font-extrabold font-manrope ${
+        className={`bg-primary text-white py-3 px-8 rounded-[12px] transition flex items-center justify-center font-extrabold font-manrope ${
           !isFormValid || loading
             ? "opacity-50 cursor-not-allowed"
             : "hover:bg-red-800"
