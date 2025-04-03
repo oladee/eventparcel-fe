4708ac4689d6160570990d0e3cb071f05f7da817
henrytrust1111 <henrytrust1111@gmail.com>
2025-04-03 09:09:54 +0100

    fixing build errors

--- components/dashboard/Container.tsx
+++ components/dashboard/Container.tsx
@@ -39,6 +39,7 @@ function Container({ children }: { children: React.ReactNode }) {
         });
         // Save the user profile in localStorage
         localStorage.setItem("loggedInUser", JSON.stringify(response.data));
+        localStorage.setItem("loggedInUserEmail", response.data.email)
         console.log("User profile fetched successfully:", response.data);
       } catch (error: any) {
         console.error("Error fetching user profile:", error);
