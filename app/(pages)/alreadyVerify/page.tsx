5e587eae8a7efb63816e735d427c1dfd16084d76
henrytrust1111 <henrytrust1111@gmail.com>
2025-05-14 18:02:11 +0100

    latest push1m

--- app/(pages)/alreadyVerify/page.tsx
+++ app/(pages)/alreadyVerify/page.tsx
@@ -15,7 +15,7 @@ const AlreadyVerify = () => {
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 px-6">
         <div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full text-center">
           <h1 className="text-2xl md:text-3xl font-bold text-gray-800">
-            You're Already Verified!
+            You&apos;re Already Verified!
           </h1>
           <p className="text-gray-600 mt-4">
             Your account has already been verified. You can proceed to log in and start using our platform.
