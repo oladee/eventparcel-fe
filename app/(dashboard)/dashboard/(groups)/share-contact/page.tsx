2ed06060195800d8827b23f84765fdb6aac7c565
henrytrust1111 <henrytrust1111@gmail.com>
2025-05-15 12:42:49 +0100

    latest push1m

--- app/(dashboard)/dashboard/(groups)/share-contact/page.tsx
+++ app/(dashboard)/dashboard/(groups)/share-contact/page.tsx
@@ -8,6 +8,8 @@ import ContactModal from "@/components/shareContact/ContactModal";
 import SendContactModal from "@/components/shareContact/SendContactModal";
 import Container from "@/components/dashboard/Container";
 import { useRouter, useSearchParams } from "next/navigation";
+import Cookies from "js-cookie";
+
 
 // API contact type
 interface APICONTACT {
@@ -96,13 +98,14 @@ const ShareContact: React.FC = () => {
       setPopupLoading(true);
   
       // Extract the token from cookies
-      console.log("Cookies:", document.cookie);
-      const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
-        const [key, value] = cookie.split("=");
-        acc[key] = value;
-        return acc;
-      }, {} as Record<string, string>);
-      const googleAccessToken = cookies["googleAccessToken"];
+      // console.log("Cookies:", document.cookie);
+      // const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+      //   const [key, value] = cookie.split("=");
+      //   acc[key] = value;
+      //   return acc;
+      // }, {} as Record<string, string>);
+      // const googleAccessToken = cookies["googleAccessToken"];
+      const googleAccessToken = Cookies.get("googleAccessToken")
       
       if (!googleAccessToken) {
         console.error("Google access token is missing in cookies");
