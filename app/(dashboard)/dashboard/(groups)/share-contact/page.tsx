686b9cf27d678df648994ee76bd4e7715e41fa49
henrytrust1111 <henrytrust1111@gmail.com>
2025-05-16 23:01:36 +0100

    latest push

--- app/(dashboard)/dashboard/(groups)/share-contact/page.tsx
+++ app/(dashboard)/dashboard/(groups)/share-contact/page.tsx
@@ -154,6 +154,20 @@ const ShareContact: React.FC = () => {
   useEffect(() => {
     if (popUpParam === "true") {
       setPopupLoading(true);
+
+      const cookies = document.cookie.split("; ").reduce((acc, cookie) => {
+        const [key, value] = cookie.split("=");
+        acc[key] = value;
+        return acc;
+      }, {} as Record<string, string>);
+      const googleAccessToken = cookies["googleAccessToken"];
+  
+      if (!googleAccessToken) {
+        console.error("Google access token is missing in cookies");
+        setPopupError("Authentication token is missing. Please log in again.");
+        setPopupLoading(false);
+        return;
+      }
   
       fetch("https://api-eventparcel.onrender.com/auth/fetch-contacts", {
         method: "GET",
