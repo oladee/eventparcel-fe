9b20fd36657a3655e04b414daadf9004aa48abc6
henrytrust1111 <henrytrust1111@gmail.com>
2025-05-20 13:44:27 +0100

    latest push

--- components/dashboard/Container.tsx
+++ components/dashboard/Container.tsx
@@ -46,6 +46,7 @@ function Container({ children }: { children: React.ReactNode }) {
         localStorage.setItem("loggedInUser", JSON.stringify(response.data));
         localStorage.setItem("loggedInUserEmail", response.data.email)
         localStorage.setItem("loggedInUserId", response.data.hostId._id)
+        console.log("loggedInUserId", response.data.hostId._id)
         console.log("User profile fetched successfully:", response.data);
       } catch (error: any) {
         console.error("Error fetching user profile:", error);
