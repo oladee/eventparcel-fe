a5bee14ddc4fdb93b495816e990e78bc5d232f4e
henrytrust1111 <henrytrust1111@gmail.com>
2025-07-19 01:04:02 +0100

    updates

--- app/(pages)/pickup-details/PickupDetailsContent.tsx
+++ app/(pages)/pickup-details/PickupDetailsContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useState, useEffect } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import DatePicker from "react-datepicker";
