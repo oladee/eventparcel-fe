f8ad53adbfc3492fd6d66ae5c267469e91bbe39d
oladee <deemajor230600@gmail.com>
2026-02-12 19:22:05 +0100

    latest update and tiny fixes

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
     dangerouslyAllowSVG: true,
     remotePatterns:[
       {
         protocol: 'https',
         hostname: '*',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ]
   },
 };
