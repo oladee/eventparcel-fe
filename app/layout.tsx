e5766a4f066b8804b3196f82409541e69e4b0420
oladee <deemajor230600@gmail.com>
2026-04-16 10:58:46 +0100

    latest update

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
         />
         {/* Facebook Pixel noscript */}
         <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             height="1"
             width="1"
